baa3062a6cfc2c6c9b62443f25044a9e64727a21
예진 박 <a2846461@gmail.com>
2024-05-06 02:41:07 +0900

    초기 커밋.

---
+++ 2012/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _2012
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(1, -2);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(130, 30);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "Game Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Location = new System.Drawing.Point(167, -2);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(130, 30);
+            this.btnstop.TabIndex = 1;
+            this.btnstop.Text = "Game Stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnstart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnstop;
+    }
+}
+
---
+++ 2012/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2012
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 1000;
+            this.Height = 800;
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            Pen pen = new Pen(Color.Black, 3);
+            Brush brush = new SolidBrush(Color.Black);
+
+            Rectangle rectOut = new Rectangle(200, 80, 600, 600);
+            Rectangle rectMid = new Rectangle(300, 180, 400, 400);
+            Rectangle rectIn = new Rectangle(400, 280, 200, 200);
+
+            Point p1 = new Point(500, 80);
+            Point p2 = new Point(500, 280);
+
+            Point p3 = new Point(200, 380);
+            Point p4 = new Point(400, 380);
+
+            Point p5 = new Point(500, 480);
+            Point p6 = new Point(500, 680);
+
+            Point p7 = new Point(600, 380);
+            Point p8 = new Point(800, 380);
+
+            Rectangle point1 = new Rectangle(194, 74, 13, 13);
+            Rectangle point2 = new Rectangle(494, 74, 13, 13);
+            Rectangle point3 = new Rectangle(794, 74, 13, 13);
+
+            Rectangle point4 = new Rectangle(294, 174, 13, 13);
+            Rectangle point5 = new Rectangle(494, 174, 13, 13);
+            Rectangle point6 = new Rectangle(694, 174, 13, 13);
+
+            Rectangle point7 = new Rectangle(394, 274, 13, 13);
+            Rectangle point8 = new Rectangle(494, 274, 13, 13);
+            Rectangle point9 = new Rectangle(594, 274, 13, 13);
+
+            Rectangle point10 = new Rectangle(194, 374, 13, 13);
+            Rectangle point11 = new Rectangle(294, 374, 13, 13);
+            Rectangle point12 = new Rectangle(394, 374, 13, 13);
+            Rectangle point13 = new Rectangle(594, 374, 13, 13);
+            Rectangle point14 = new Rectangle(694, 374, 13, 13);
+            Rectangle point15 = new Rectangle(794, 374, 13, 13);
+
+            Rectangle point16 = new Rectangle(394, 474, 13, 13);
+            Rectangle point17 = new Rectangle(494, 474, 13, 13);
+            Rectangle point18 = new Rectangle(594, 474, 13, 13);
+
+            Rectangle point19 = new Rectangle(294, 574, 13, 13);
+            Rectangle point20 = new Rectangle(494, 574, 13, 13);
+            Rectangle point21 = new Rectangle(694, 574, 13, 13);
+
+            Rectangle point22 = new Rectangle(194, 674, 13, 13);
+            Rectangle point23 = new Rectangle(494, 674, 13, 13);
+            Rectangle point24 = new Rectangle(794, 674, 13, 13);
+
+            g.DrawRectangle(pen, rectOut);
+            g.DrawRectangle(pen, rectMid);
+            g.DrawRectangle(pen, rectIn);
+
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p3, p4);
+            g.DrawLine(pen, p5, p6);
+            g.DrawLine(pen, p7, p8);
+
+            g.DrawEllipse(pen, point1);
+            g.DrawEllipse(pen, point2);
+            g.DrawEllipse(pen, point3);
+            g.DrawEllipse(pen, point4);
+            g.DrawEllipse(pen, point5);
+            g.DrawEllipse(pen, point6);
+            g.DrawEllipse(pen, point7);
+            g.DrawEllipse(pen, point8);
+            g.DrawEllipse(pen, point9);
+            g.DrawEllipse(pen, point10);
+            g.DrawEllipse(pen, point11);
+            g.DrawEllipse(pen, point12);
+            g.DrawEllipse(pen, point13);
+            g.DrawEllipse(pen, point14);
+            g.DrawEllipse(pen, point15);
+            g.DrawEllipse(pen, point16);
+            g.DrawEllipse(pen, point17);
+            g.DrawEllipse(pen, point18);
+            g.DrawEllipse(pen, point19);
+            g.DrawEllipse(pen, point20);
+            g.DrawEllipse(pen, point21);
+            g.DrawEllipse(pen, point22);
+            g.DrawEllipse(pen, point23);
+            g.DrawEllipse(pen, point24);
+
+            g.FillEllipse(brush, point1);
+            g.FillEllipse(brush, point2);
+            g.FillEllipse(brush, point3);
+            g.FillEllipse(brush, point4);
+            g.FillEllipse(brush, point5);
+            g.FillEllipse(brush, point6);
+            g.FillEllipse(brush, point7);
+            g.FillEllipse(brush, point8);
+            g.FillEllipse(brush, point9);
+            g.FillEllipse(brush, point10);
+            g.FillEllipse(brush, point11);
+            g.FillEllipse(brush, point12);
+            g.FillEllipse(brush, point13);
+            g.FillEllipse(brush, point14);
+            g.FillEllipse(brush, point15);
+            g.FillEllipse(brush, point16);
+            g.FillEllipse(brush, point17);
+            g.FillEllipse(brush, point18);
+            g.FillEllipse(brush, point19);
+            g.FillEllipse(brush, point20);
+            g.FillEllipse(brush, point21);
+            g.FillEllipse(brush, point22);
+            g.FillEllipse(brush, point23);
+            g.FillEllipse(brush, point24);
+
+            pen.Dispose();
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
